c8ef540205010a68e74d8d2a66ee1400f8cbbc5b
angelgbcore <88762214+angelgbcore@users.noreply.github.com>
2021-12-20 17:37:52 -0600

    05 - reactDOM se ve la funcionalidad de react dom al renderizar los elementos

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import { StrictMode } from "react";
+// importar la libreria de react
+import React from "react";
+// importar el manejador de dom react
 import ReactDOM from "react-dom";
 
-import App from "./App";
+// obtener el elemento raiz del index.html
+const root = document.getElementById("root");
+// definir el elemento a incrustar en la raiz
+var element = <h1>hola mundo</h1>;
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+// renderizar con el metodo de reactDOM
+// ReactDOM.render(__que__, __donde__);
+ReactDOM.render(element, root);
