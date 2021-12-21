69a38d936250c5722d8c3218f5ab0c83294db403
angelgbcore <88762214+angelgbcore@users.noreply.github.com>
2021-12-20 18:20:04 -0600

    06 - JSX conocer cómo funciona JSX (etiquetas html dentro de js)

--- src/index.js
+++ src/index.js
@@ -5,9 +5,33 @@ import ReactDOM from "react-dom";
 
 // obtener el elemento raiz del index.html
 const root = document.getElementById("root");
-// definir el elemento a incrustar en la raiz
-var element = <h1>hola mundo</h1>;
+
+var usuario = {
+  name: "Angel",
+  apellido: "Barrera"
+}
+function getData(usuario) {
+  return `${usuario.name} ${usuario.apellido}`
+}
+
+// --------------------------------------------------
+// JSX es una manera de poner etiquetas HTML dentro
+// de codigo javascript para mejor renderizado del DOM y para
+// mejorar la visualizacion del codigo
+// --------------------------------------------------
+
+// dentro de los componentes JSX se pueden insertar expresiones
+// variables, funciones, etc
+var element = <h1>Hola {getData(usuario)}</h1>;
+
+// tambien se pueden anidar las etiquetas poniendo dos parentesis
+var element_two = (
+  <div>
+    <h4>Hola {usuario.name}</h4>
+    <p>Hola {getData(usuario)}</p>
+  </div>
+)
 
 // renderizar con el metodo de reactDOM
 // ReactDOM.render(__que__, __donde__);
-ReactDOM.render(element, root);
+ReactDOM.render([element, element_two], root);
